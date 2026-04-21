7c6351c451ac32b51bf09e9048cee96cba195062
sameepabadhuge <sithusameepa@gmail.com>
2026-04-21 23:53:20 +0530

    create google auth login

---
+++ config/passport.js
@@ -0,0 +1,68 @@
+// backend/config/passport.js
+
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("../models/User");
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL:
+        "http://localhost:5000/api/auth/google/callback",
+    },
+
+    async (
+      accessToken,
+      refreshToken,
+      profile,
+      done
+    ) => {
+      try {
+        const email =
+          profile.emails[0].value;
+
+        let user = await User.findOne({
+          email,
+        });
+
+        // If user doesn't exist, create new user
+        if (!user) {
+          user = await User.create({
+            name: profile.displayName,
+            email: email,
+            googleId: profile.id,
+            role: "user",
+          });
+        }
+
+        return done(null, user);
+
+      } catch (error) {
+        return done(error, null);
+      }
+    }
+  )
+);
+
+// Session Support
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(
+  async (id, done) => {
+    try {
+      const user =
+        await User.findById(id);
+
+      done(null, user);
+
+    } catch (error) {
+      done(error, null);
+    }
+  }
+);
+
+module.exports = passport;
--- server.js
+++ server.js
@@ -1,26 +1,36 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const cors = require("cors");
-const connectDB = require("./config/db");
 
 dotenv.config();
+
+const cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
+
+const connectDB = require("./config/db");
+require("./config/passport");
+
 connectDB();
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
 
-// Routes
+app.use(express.json());
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+
 app.use("/api/auth", require("./routes/authRoutes"));
 
-app.get("/", (req, res) => {
-  res.send("FreshCart API Running...");
-});
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server running...");
 });
