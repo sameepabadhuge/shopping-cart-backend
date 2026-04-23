190e8bd82f1f5a39061e0cc59fc92f83a6cf2487
sameepabadhuge <sithusameepa@gmail.com>
2026-04-23 09:11:50 +0530

    update product ui and backend

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,20 +1,34 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
 
 const {
- addProduct,
- getProducts,
- deleteProduct,
- updateProduct
+  addProduct,
+  getProducts,
+  deleteProduct,
+  updateProduct,
 } = require("../controllers/productController");
 
+// Storage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
 
-router.post("/", addProduct);
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+// Routes
+router.post("/", upload.single("image"), addProduct);
 
 router.get("/", getProducts);
 
 router.delete("/:id", deleteProduct);
 
-router.put("/:id", updateProduct);
+router.put("/:id", upload.single("image"), updateProduct);
 
 module.exports = router;
