0efdb43ad8448714ab3e317d0c53f867e271514b
sameepabadhuge <sithusameepa@gmail.com>
2026-04-24 09:54:37 +0530

    connect product details and backend,to fetch real product details

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,45 +1,58 @@
-const Product =
-require("../models/Product");
+const Product = require("../models/Product");
 
 
 // Add Product
 exports.addProduct = async (req, res) => {
   try {
-
-    const product =
-      await Product.create({
-        name: req.body.name,
-        category: req.body.category,
-        price: req.body.price,
-        stock: req.body.stock,
-        description: req.body.description,
-        image: req.file ? req.file.filename : ""
-      });
+    const product = await Product.create({
+      name: req.body.name,
+      category: req.body.category,
+      price: req.body.price,
+      stock: req.body.stock,
+      description: req.body.description,
+      image: req.file ? req.file.filename : "",
+    });
 
     res.status(201).json(product);
-
   } catch (error) {
     res.status(500).json({
-      message: error.message
+      message: error.message,
     });
   }
 };
 
 
-// Get Products
+// Get All Products
 exports.getProducts = async (req, res) => {
   try {
-
-    const products =
-      await Product.find().sort({
-        createdAt: -1
-      });
+    const products = await Product.find().sort({
+      createdAt: -1,
+    });
 
     res.json(products);
-
   } catch (error) {
     res.status(500).json({
-      message: error.message
+      message: error.message,
+    });
+  }
+};
+
+
+// Get Single Product
+exports.getSingleProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({
+        message: "Product not found",
+      });
+    }
+
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
     });
   }
 };
@@ -48,18 +61,14 @@ exports.getProducts = async (req, res) => {
 // Delete Product
 exports.deleteProduct = async (req, res) => {
   try {
-
-    await Product.findByIdAndDelete(
-      req.params.id
-    );
+    await Product.findByIdAndDelete(req.params.id);
 
     res.json({
-      message: "Deleted Successfully"
+      message: "Deleted Successfully",
     });
-
   } catch (error) {
     res.status(500).json({
-      message: error.message
+      message: error.message,
     });
   }
 };
@@ -68,31 +77,28 @@ exports.deleteProduct = async (req, res) => {
 // Update Product
 exports.updateProduct = async (req, res) => {
   try {
-
     const updateData = {
       name: req.body.name,
       category: req.body.category,
       price: req.body.price,
       stock: req.body.stock,
-      description: req.body.description
+      description: req.body.description,
     };
 
     if (req.file) {
       updateData.image = req.file.filename;
     }
 
-    const product =
-      await Product.findByIdAndUpdate(
-        req.params.id,
-        updateData,
-        { new: true }
-      );
+    const product = await Product.findByIdAndUpdate(
+      req.params.id,
+      updateData,
+      { new: true }
+    );
 
     res.json(product);
-
   } catch (error) {
     res.status(500).json({
-      message: error.message
+      message: error.message,
     });
   }
 };
